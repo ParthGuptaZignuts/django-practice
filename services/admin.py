fa4a1f3756ee78da5b5105ff16506fb7aec9ccd2
Zignuts Parth Gupta <parthg@zignuts.com>
2024-07-29 17:47:56 +0530

    created new app for learning migration

---
+++ services/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from services.models import Service
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('service_icon', 'service_title', 'service_description')
+
+admin.site.register(Service, ServiceAdmin)
+
