8c51cea9fe18a6e4a458c25e106b13682ac10f85
Zignuts Parth Gupta <parthg@zignuts.com>
2024-07-29 16:37:03 +0530

    custom validation

--- learningDjango/forms.py
+++ learningDjango/forms.py
@@ -1,4 +1,11 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 class NumberForm(forms.Form):
     num = forms.IntegerField(label="Enter the number to check:")
+
+    def clean_num(self):
+        number = self.cleaned_data['num']
+        if number < 0:
+            raise ValidationError("The number must be non-negative.")
+        return number
